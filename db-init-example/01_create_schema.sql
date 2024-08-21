10de8922c4650d25c095685c4681aa3b6aaed1e2
Nicolás Sañudo <nicolas.sanudo2@gmail.com>
2024-08-21 20:19:20 -0300

    ⚰️ refactor(postgres): remove test code
    - clean up SQL file by removing unnecessary code

--- db-init-example/01_create_schema.sql
+++ db-init-example/01_create_schema.sql
@@ -10,20 +10,3 @@ CREATE TABLE usuarios (
   datos_usuarios_id INT NOT NULL UNIQUE,
   CONSTRAINT fk_datos_usuarios_id FOREIGN KEY (datos_usuarios_id) REFERENCES datos_usuarios(id)
 );
-
-INSERT INTO datos_usuarios(libros_en_posesion) VALUES (
-  '[
-  {"id": "0001", "fecha-prestamo": "01-01-2001"},
-  {"id": "0010", "fecha-prestamo": "02-02-2002"}
-  ]'
-);
-INSERT INTO usuarios(nombres, apellidos, datos_usuarios_id) VALUES ('kimbo', 'berna', 1);
-
-SELECT 
-  usuarios.nombres,
-  usuarios.apellidos,
-  libro->>'id' AS id_libro,
-  libro->>'fecha-prestamo' AS fecha_prestamo
-FROM usuarios JOIN datos_usuarios
-ON usuarios.datos_usuarios_id = datos_usuarios.id,
-jsonb_array_elements(datos_usuarios.libros_en_posesion) AS libro;
