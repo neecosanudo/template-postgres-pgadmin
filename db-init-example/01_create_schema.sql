03479c1b8639e1e9b191b0bbe979118f5bd5cea8
Nicolás Sañudo <nicolas.sanudo2@gmail.com>
2024-08-21 21:11:45 -0300

    ✨ feat(postgres): add tables for books and loans
    - create 'libros' and 'prestamos_libros' tables
    - remove unnecessary 'libros_en_posesion' column from 'datos_usuarios'

--- db-init-example/01_create_schema.sql
+++ db-init-example/01_create_schema.sql
@@ -1,6 +1,5 @@
 CREATE TABLE datos_usuarios (
-  id SERIAL PRIMARY KEY,
-  libros_en_posesion JSONB
+  id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE usuarios (
@@ -9,4 +8,19 @@ CREATE TABLE usuarios (
   apellidos VARCHAR(24) NOT NULL,
   datos_usuarios_id INT NOT NULL UNIQUE,
   CONSTRAINT fk_datos_usuarios_id FOREIGN KEY (datos_usuarios_id) REFERENCES datos_usuarios(id)
+);
+
+CREATE TABLE libros (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE prestamos_libros (
+  id SERIAL PRIMARY KEY,
+  usuario_id INT NOT NULL,
+  libro_id INT NOT NULL,
+  fecha_prestamo TIMESTAMP DEFAULT NOW(),
+  
+  CONSTRAINT fk_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+  CONSTRAINT fk_libro_id FOREIGN KEY (libro_id) REFERENCES libros(id)  
 );
